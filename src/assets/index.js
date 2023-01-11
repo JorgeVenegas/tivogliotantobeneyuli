e459805acbd63695ed0cc3300757eedc2e275d91
Jorge Venegas <veljorge@live.com>
2023-01-11 00:10:57 -0600

    Migration 5 Jan

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,6 +4,13 @@ import espejo from "./espejo.jpg"
 import dic31 from "./dic31.jpg";
 import dic31_2 from "./dic31_2.jpeg";
 import ene1 from "./ene1.png";
+import ene2 from "./ene2.png";
+import ene2_2 from "./ene2_2.jpg";
+import ene3 from "./ene3.jpg";
+import ene4 from "./ene4.png";
+import ene4_2 from "./ene4_2.jpeg";
+import ene5 from "./ene5.jpg";
+import ene5_2 from "./ene5_2.png";
 
 export {
   menu,
@@ -12,4 +19,11 @@ export {
   dic31,
   dic31_2,
   ene1,
+  ene2,
+  ene2_2,
+  ene3,
+  ene4,
+  ene4_2,
+  ene5,
+  ene5_2,
 };
--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -1,6 +1,6 @@
 import styles from "../style";
 
-const Day = ({ name, day, title, paragraphs, images, bgColor }) => {
+const Day = ({ name, day, title, paragraphs, images, bgColor, reverse }) => {
 
     return (
         <div className={`${bgColor} ${styles.flexStart} z-0`}>
@@ -12,7 +12,7 @@ const Day = ({ name, day, title, paragraphs, images, bgColor }) => {
                             <mark className="bg-primary text-light rounded">{day}</mark>
                         </p>
                     </div>
-                    <div className={`${name === "prox" ? "hidden" : ""} flex md:flex-row flex-col mt-8`}>
+                    <div className={`${name === "prox" ? "hidden" : ""} ${reverse ? "md:flex-row-reverse" : "md:flex-row"} flex flex-col mt-8`}>
                         <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
                             <div className="flex flex-row justify-between items-center w-full">
                                 <h1 className="flex-1 font-punk font-normal ss:text-[80px] text-[60px] text-light ss:leading-[55px] leading-[40px] mb-5">
@@ -53,7 +53,7 @@ const Day = ({ name, day, title, paragraphs, images, bgColor }) => {
                                 <img src={image} className={`w-full h-auto sm:py-16 py-6 ${image ? "" : "hidden"}`} />
                             )) : ""
                             }
-                            <p className={`${styles.paragraph} text-light font-night font-extralight ss:text-[24px] text-[18px] mt-5`}>
+                            <p className={`${styles.paragraph} text-light font-night font-extralight ss:text-[24px] text-[18px] mt-5 ${name === "today" ? "" : "hidden"}`}>
                                 {"Vuelve mañana, "} <mark className="bg-primary text-light rounded">{"ti voglio tanto bene, Yuli."}</mark>
                             </p>
                         </div>
